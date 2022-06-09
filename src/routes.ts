e6f5a04dd8ef05712dbb60d38d52bd2032396bc7
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-06-09 14:20:13 +0530

    Documenting routes better

--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,10 @@ import shoppingFlowRouteHandler from './shoppingFlowRouteHandler'
 
 export default new Router()
 
-  // Layer0 files
-  .match('/service-worker.js', ({ serviceWorker }) => serviceWorker('dist/service-worker.js'))
+  // Layer0 Service Worker Files
+  .match('/service-worker.js', ({ serviceWorker }) => {
+    serviceWorker('dist/service-worker.js')
+  })
   .match('/l0_main.js', ({ serveStatic, cache }) => {
     cache(CACHE_ASSETS)
     serveStatic('dist/browser.js')
@@ -13,20 +15,28 @@ export default new Router()
 
   // Home page
   .match('/', shoppingFlowRouteHandler)
+
+  // Matches pages nested to a one subroute
+  // For e.g. /something/, /oops/
   .match('/:suffix/', shoppingFlowRouteHandler)
 
   // PLP page
+  // Matches pages nested to any subroute inside /:suffix/w/
+  // For e.g. /something/w/oops/haha/lol, /something/w/oops/haha/joking
   .match('/:suffix/w/:path*', shoppingFlowRouteHandler)
+
+  // Matches pages nested to any subroute inside /w/
+  // For e.g. //w/oops/haha/lol, /w/oops/haha/joking
   .match('/w/:path*', shoppingFlowRouteHandler)
 
   // PDP page
   .match('/:suffix/launch/:path*', shoppingFlowRouteHandler)
   .match('/launch/:path*', shoppingFlowRouteHandler)
 
-  // example route for cacheable assets:
+  // Example route for cacheable assets
   .match('/images/:path*', ({ cache, proxy }) => {
     cache(CACHE_ASSETS)
-    return proxy('origin')
+    proxy('origin')
   })
 
   // .match('/l0-api/:path*', ({ cache, proxy }) => {
@@ -43,10 +53,11 @@ export default new Router()
   //   })
   // })
 
-  // If not found at any of above, but is an asset, cache it.
+  // Matches any route that ends with js|mjs|css|png|ico|svg|jpg|jpeg|gif|ttf|woff|otf
+  // Remove set-cookie (and updates cache-control) header to be able to cache these assets
   .match(
     '/:path*/:file.:ext(js|mjs|css|png|ico|svg|jpg|jpeg|gif|ttf|woff|otf)',
-    ({ cache, removeUpstreamResponseHeader, proxy, setResponseHeader }) => {
+    ({ cache, proxy, removeUpstreamResponseHeader, setResponseHeader }) => {
       setResponseHeader('cache-control', 'public, max-age=86400')
       removeUpstreamResponseHeader('set-cookie')
       cache(CACHE_ASSETS)
@@ -54,7 +65,7 @@ export default new Router()
     }
   )
 
-  // fallback route for all other requests:
+  // fallback route to origin for all other requests
   .fallback(({ proxy }) => {
     proxy('origin')
   })

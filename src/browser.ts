7d815791b8b3ed7534d8686e8f03992165781942
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-06-09 13:12:59 +0530

    Add devtools

--- src/browser.ts
+++ src/browser.ts
@@ -1,6 +1,8 @@
 import install from '@layer0/prefetch/window/install'
+// import installDevtools from '@layer0/devtools/install'
 
 document.addEventListener('DOMContentLoaded', function () {
   // @ts-ignore
   install()
+  // installDevtools()
 })
